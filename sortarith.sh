6a990b25bd16d83e3faca866d1c7251478216406
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-27 13:13:40 +0530

    ADDED:Operation2

--- sortarith.sh
+++ sortarith.sh
@@ -2,6 +2,7 @@ read -p "Enter the first input: " a
 read -p "Enter the second input: " b
 read -p "Enter the third input: " c
 
-
 OPERATION1=$(($a+$b*$c))
-echo $OPERATION1
+OPERATION2=$(($a*$b+$c))
+echo "first operation" $OPERATION1
+echo "second operation" $OPERATION2
