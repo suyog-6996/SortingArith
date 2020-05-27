373e0a155508cd79f703573f9cbd6879872e8cab
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-27 13:35:30 +0530

    ADDED:Operation4

--- sortarith.sh
+++ sortarith.sh
@@ -5,6 +5,8 @@ read -p "Enter the third input: " c
 OPERATION1=$(($a+$b*$c))
 OPERATION2=$(($a*$b+$c))
 OPERATION3=$(($c+$a/$b))
+OPERATION4=$(($a%$b+$c))
 echo "first operation" $OPERATION1
 echo "second operation" $OPERATION2
 echo "Third operation" $OPERATION3
+echo "Fourth operation" $OPERATION4
